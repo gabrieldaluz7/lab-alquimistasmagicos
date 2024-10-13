23234efe0e02f1ca5341aff27db3b4d179130817
Gabriel Barbosa <gabrielbarbosaweb7@gmail.com>
2024-10-13 16:34:38 -0400

    Update Code

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,19 +11,20 @@
     <nav class="navbar">
         <ul>
             <li><a href="#">Início</a></li>
-            <li><a href="#">Carimbos</a></li>
+            <!-- <li><a href="#">Carimbos</a></li>
             <li><a href="#">Cultura Sólida</a></li>
             <li><a href="#">Cultura Líquida</a></li>
             <li><a href="#">Spawn</a></li>
             <li><a href="#">Monotube</a></li>
-            <li><a href="#">Casing</a></li>
+            <li><a href="#">Casing</a></li> -->
             <li><a href="{{ route('logout') }}">Sair</a></li>
         </ul>
     </nav>
 
     <!-- Container principal -->
     <div class="dashboard-container">
-        <h1>Dashboard</h1>
+
+        <!-- <h1>Dashboard</h1> -->
 
          <!-- Tabela adicionada -->
         <table class="dashboard-table">
@@ -36,57 +37,14 @@
                 </tr>
             </thead> -->
             <tbody>
+                
                 <tr>
                     <td>Carimbos</td>
                     <td>15</td>
            
 
                 </tr>
-                <tr>
-                    <td>Seringa de Esporos</td>
-                    <td>15</td>
-           
-
-                </tr>
-                <tr>
-                    <td>Swab</td>
-                    <td>8</td>
-           
-
-                </tr>
-                <tr>
-                    <td>Cultura Sólida</td>
-                    <td>25</td>
-           
-
-                </tr>
-                <tr>
-                    <td>Cultura Liquida</td>
-                    <td>0</td>
-           
-
-                </tr>
-
-                <tr>
-                    <td>Spawn</td>
-                    <td>8</td>
-           
-
-                </tr>
-
-                <tr>
-                    <td>Monotube</td>
-                    <td>6</td>
-           
-
-                </tr>
-
-                <tr>
-                    <td>Casing</td>
-                    <td>0</td>
-           
-
-                </tr>
+              
 
 
                 <!-- Adicione mais linhas conforme necessário -->
